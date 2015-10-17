6fe1ccf7d96ba48e70670db1edacc4ff41b75b0e
Ivan Potapenko <potapenko.ivan@gmail.com>
2015-10-17 15:25:17 +0600

    Add getting current work day method.

--- software/pg-web/pg-web/sys/pg/WorkDayModule.cs
+++ software/pg-web/pg-web/sys/pg/WorkDayModule.cs
@@ -10,12 +10,32 @@ namespace pg_web.sys.pg
 	class WorkDayModule : IModule
 	{
 		public const String SERVICE_NAME = "WorkDay";
-
 		private pgworkDBEntities db;
 
 		public void init()
 		{
 			db = new pgworkDBEntities();
+			try
+			{
+				currentWorkDay = (
+					from m in db.WorkDays
+					where m.workDayState == WorkDayState.wdstInProgress
+					select m
+				).First<WorkDay>();
+			}
+			catch (Exception)
+			{
+				currentWorkDay = null;
+			}
+
+			_initTimers();
+		}
+
+		public WorkDay currentWorkDay { get; private set; }
+
+		private void _initTimers()
+		{
+
 		}
 	}
 }
