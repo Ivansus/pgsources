73ccd18998ba95dc35b39144e55d61b7ea5407d8
Ivan Potapenko <potapenko.ivan@gmail.com>
2015-11-04 21:00:39 +0600

    Process area visit event and generate work area visit event.

--- software/pg-web/pg-web/sys/pg/WorkAreaModule.cs
+++ software/pg-web/pg-web/sys/pg/WorkAreaModule.cs
@@ -8,13 +8,24 @@ namespace pg_web.sys.pg
 {
 	public class WorkAreaVisitEvent : EventArgs
 	{
+		public WorkAreaVisitEvent(WorkDay _workDay, WorkArea _workArea, AreaVisitEvent _event)
+		{
+			workDay = _workDay;
+			workArea = _workArea;
+			device = _event.device;
+			areaEvent = _event;
+		}
 
+		public WorkDay workDay { get; }
+		public WorkArea workArea { get; }
+		public Device device { get; }
+		public AreaVisitEvent areaEvent { get; }
 	}
 
 	public class WorkAreaModule : IModule
 	{
 		public const String SERVICE_NAME = "WorkAreaModule";
-		public event EventHandler<DeviceLabelEvent> WorkAreaVisitEventHandler;
+		public event EventHandler<WorkAreaVisitEvent> WorkAreaVisitEventHandler;
 
 		private pgworkDBEntities db;
 		private WorkDayModule workDayModule;
@@ -29,10 +40,28 @@ namespace pg_web.sys.pg
 			areaModule.AreaVisitEventHandler += _workAreaModule_AreaVisitEvent;
 		}
 
-		private void _workAreaModule_AreaVisitEvent(object sender, AreaVisitEvent e)
+		private void _workAreaModule_AreaVisitEvent(object _sender, AreaVisitEvent _e)
 		{
 			WorkDay wd = workDayModule.currentWorkDay;
-
+			if (wd == null)
+			{
+				System.Diagnostics.Debug.Write("Area visit event with no started work day.");
+				return;
+			}
+			try
+			{
+				WorkArea wa = (
+					from m in db.WorkAreas
+					where m.workDayId == wd.workDayId &&
+					m.areaId == _e.area.areaId
+					select m
+				).First<WorkArea>();
+				WorkAreaVisitEventHandler(this, new WorkAreaVisitEvent(wd, wa, _e));
+			}
+			catch (Exception)
+			{
+				System.Diagnostics.Debug.Write("There is no work area associated with '" + _e.area.areaName +"' in current work day (" + wd.workDayId + ")");
+			}
 		}
 	}
 }
